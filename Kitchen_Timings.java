ccb618094bdac9d3becb790bb387b83936273e13
Shreya Nalawade <shreyanalawade882@gmail.com>
2023-06-20 21:42:59 +0530

    Create Kitchen_Timings.java

---
+++ Kitchen_Timings.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn=new Scanner(System.in);
+		int t=scn.nextInt();
+		for (int i=0;i<t;i++){
+		    int x=scn.nextInt();
+		    int y=scn.nextInt();
+		    System.out.println(y-x);
+		}// your code goes here
+	}
+}
