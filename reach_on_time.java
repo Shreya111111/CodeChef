b751014616dd594697626a8fe861ff510b7a28db
Shreya Nalawade <shreyanalawade882@gmail.com>
2023-06-20 21:50:22 +0530

    Create reach_on_time.java

---
+++ reach_on_time.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    for(int i=0;i<n;i++){
+	        int x=s.nextInt();
+	        if(x>=30)
+	        System.out.println("yes");
+	        else
+            System.out.println("no");
+	    }
+		// your code goes here
+	}
+}
